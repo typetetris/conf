ad216df27b7fd6b65cd8804fad70a5a88c23c471
Eric Wolf <eric.wolf@lindenbaum.eu>
2020-03-07 15:38:23 +0100

    Changes in the config.

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -3,91 +3,197 @@ let
     url    = "https://github.com/infinisil/all-hies/tarball/92148680060ed68f24738128d8489f4f9387d2ff";
     sha256 = "1yb75f8p09dp4yx5d3w3wvdiflaav9a5202xz9whigk2p9ndwbp5";
   }) {};
+  b9ConfigFile = lxc_emulator: network:
+    ''
+    [global]
+    build_dir_root: Nothing
+    environment_vars: []
+    exec_env: LibVirtLXC
+    keep_temp_dirs: False
+    log_file: Nothing
+    profile_file: Nothing
+    repository: Nothing
+    repository_cache: Just (InB9UserDir "repo-cache")
+    unique_build_dirs: True
+    max_cached_shared_images: Nothing
+    verbosity: Just LogTrace
+    
+    [libvirt-lxc]
+    use_sudo: False
+    emulator_path: Just "${lxc_emulator}"
+    connection: lxc:///
+    network: ${network}
+    guest_capabilities: [CAP_MKNOD,CAP_SYS_ADMIN,CAP_SYS_CHROOT,CAP_SETGID,CAP_SETUID,CAP_NET_BIND_SERVICE,CAP_SETPCAP,CAP_SYS_PTRACE,CAP_SYS_MODULE]
+    guest_ram_size: RamSize 6 GB
+    
+    [tilia-repo]
+    remote_path: /var/lib/jenkins/.b9/repo-cache/local-repo
+    ssh_priv_key_file: /home/wolferic/.ssh/id_ed25519
+    ssh_remote_host: tilia
+    ssh_remote_port: 22
+    ssh_remote_user: jenkins
+    '';
 in
-{
-  allowUnfree = true;
-  packageOverrides = pkgs: with pkgs; rec {
-    myNeovim = neovim.override {
-      configure = {
-        customRC = ''
-          set hidden
-          set termguicolors
-          set diffopt=vertical,filler
-          colorscheme base16-classic-light
-          autocmd FileType haskell setlocal expandtab
-          set list
-          set listchars=tab:>-
-          let g:ale_sign_column_always=1
-          set expandtab
-          let g:ale_echo_msg_format = '[%linter%] %s'
-          let g:ale_linters = { 'haskell': ['hie'] }
+  {
+    allowUnfree = true;
+    packageOverrides = pkgs: with pkgs; rec {
+
+      myEmacsConfig = writeText "default.el"
+        ''
+          (global-set-key (kbd "C-c l") 'org-store-link)
+          (global-set-key (kbd "C-c a") 'org-agenda)
+          (global-set-key (kbd "C-c c") 'org-capture)
         '';
-        packages.myVimPackage = with pkgs.vimPlugins; {
-          # see examples below how to use custom packages
-          start = [ fugitive base16-vim vim-nix ale ];
-          # If a vim plugin has a dependency that is not explicitly listed in
-          # opt that dependency will always be added to start to avoid confusion.
-          opt = [ ];
+      myEmacs = emacsWithPackages (epkgs: (
+        [
+          (runCommand "default.el" {}
+           ''
+             mkdir -p $out/share/emacs/site-lisp
+             cp ${myEmacsConfig} $out/share/emacs/site-lisp/default.el
+           '')
+          epkgs.org
+        ] ++ (with epkgs.melpaStablePackages; [
+        ])));
+
+      myNeovim = neovim.override {
+        configure = {
+          customRC = ''
+            set hidden
+            set termguicolors
+            set diffopt=vertical,filler
+            colorscheme base16-classic-light
+            autocmd FileType haskell setlocal expandtab
+            set list
+            set listchars=tab:>-
+            let g:ale_sign_column_always=1
+            set expandtab
+            let g:ale_echo_msg_format = '[%linter%] %s'
+            let g:ale_linters = { 'haskell': ['hie'] }
+          '';
+          packages.myVimPackage = with pkgs.vimPlugins; {
+            # see examples below how to use custom packages
+            start = [ fugitive base16-vim vim-nix ale ];
+            # If a vim plugin has a dependency that is not explicitly listed in
+            # opt that dependency will always be added to start to avoid confusion.
+            opt = [ ];
+          };
         };
       };
+      robotoTestcase = pkgs.buildEnv {
+        name = "robotoTestcase";
+        paths = [
+          roboto-mono
+          ];
+        pathsToLink = [ "/share" "/bin" "/etc" ];
+        extraOutputsToInstall = [ "man" "doc" "info" ];
+        postBuild = ''
+          if [ -x $out/bin/install-info -a -w $out/share/info ]; then
+            shopt -s nullglob
+            for i in $out/share/info/*.info $out/share/info/*.info.gz; do
+                $out/bin/install-info $i $out/share/info/dir
+            done
+          fi
+        '';
+      };
+
+      b9ConfigFileS = writeText "myB9ConfigFile" (b9ConfigFile "${pkgs.libvirt.out}/libexec/libvirt_lxc" "Just \"default\"");
+      b9ConfigFileHostnetS = writeText "myB9ConfigFile" (b9ConfigFile "${pkgs.libvirt.out}/libexec/libvirt_lxc" "Nothing");
+
+      myPackages =
+        let b9 = import
+          (pkgs.fetchFromGitHub {
+            owner = "sheyll";
+            repo = "b9-vm-image-builder";
+            rev = "0.5.69.0";
+            sha256 = "1c37haid9nwzqa41brrqirqpgzbynfmfm88l272nijc0pqgik9bj";
+          }) {inherit pkgs;};
+        in
+        pkgs.buildEnv {
+        name = "myPackages";
+        paths = [
+          (runCommand "b9ConfigFileS" {} ''
+            mkdir -p $out/etc/b9
+            cp ${b9ConfigFileS} $out/etc/b9/b9.conf
+            '')
+          (runCommand "b9ConfigFileS" {} ''
+            mkdir -p $out/etc/b9
+            cp ${b9ConfigFileHostnetS} $out/etc/b9/b9.conf.hostnet
+            '')
+          xlibs.xdpyinfo
+          b9
+          bc
+          firefox
+          chromium
+          gitFull
+          keepassxc
+          killall
+          mkpasswd
+          mupdf
+          ripgrep
+          simple-scan
+          thunderbird
+          usbutils
+          yubikey-manager-qt
+          yubikey-personalization
+          yubikey-personalization-gui
+          yubico-pam
+          myNeovim
+          gnupg
+          pavucontrol
+          shellcheck
+          idea.idea-community
+          jdk
+          kotlin
+          stack
+          jq
+          myEmacs
+#          haskellPackages.ormolu
+          glxinfo
+          htop
+          cabal2nix
+          man-pages
+          pciutils
+          unzip
+          vscode
+          xsel
+          wget
+          xorg.xev
+          xscreensaver
+          arandr
+          libreoffice
+          rofi
+          virtmanager
+          libvirt.out
+          screen-message
+          vanilla-dmz
+          polybar
+          read-edid
+          meld
+          p7zip
+          postgresql_9_6
+          maven
+          dbeaver
+          gnumake
+          volumeicon
+          (import <nixos-unstable> {}).riot-desktop
+          (
+            let
+              ghcversion = "ghc${builtins.replaceStrings ["."] [""] haskellPackages.ghc.version}";
+            in
+              all-hies.selection { selector = p: { "${ghcversion}" = p."${ghcversion}"; }; }
+          )
+            
+        ];
+        pathsToLink = [ "/share" "/bin" "/etc" ];
+        extraOutputsToInstall = [ "man" "doc" "info" ];
+        postBuild = ''
+          if [ -x $out/bin/install-info -a -w $out/share/info ]; then
+            shopt -s nullglob
+            for i in $out/share/info/*.info $out/share/info/*.info.gz; do
+                $out/bin/install-info $i $out/share/info/dir
+            done
+          fi
+        '';
+      };
     };
-    myPackages = pkgs.buildEnv {
-      name = "my-packages";
-      paths = [
-        bc
-        firefox
-        chromium
-        gitFull
-        keepassxc
-        killall
-        mkpasswd
-        mupdf
-        ripgrep
-        simple-scan
-        thunderbird
-        usbutils
-        yubikey-manager-qt
-        yubikey-personalization
-        yubikey-personalization-gui
-        yubico-pam
-        myNeovim
-        gnupg
-        pavucontrol
-        networkmanagerapplet
-        shellcheck
-        idea.idea-community
-        jdk
-        kotlin
-        stack
-        jq
-        haskellPackages.ormolu
-        glxinfo
-        htop
-        cabal2nix
-        man-pages
-        pciutils
-        roboto-mono
-        unzip
-        vscode
-        xsel
-        (
-          let
-            ghcversion = "ghc${builtins.replaceStrings ["."] [""] haskellPackages.ghc.version}";
-          in
-            all-hies.selection { selector = p: { "${ghcversion}" = p."${ghcversion}"; }; }
-        )
-          
-      ];
-      pathsToLink = [ "/share/man" "/share/doc" "/share/info" "/bin" "/etc" ];
-      extraOutputsToInstall = [ "man" "doc" "info" ];
-      postBuild = ''
-        if [ -x $out/bin/install-info -a -w $out/share/info ]; then
-          shopt -s nullglob
-          for i in $out/share/info/*.info $out/share/info/*.info.gz; do
-              $out/bin/install-info $i $out/share/info/dir
-          done
-        fi
-      '';
-    };
-  };
-}
+  }
--- nixpkgs/xmonad/xmonad.hs
+++ nixpkgs/xmonad/xmonad.hs
@@ -5,16 +5,109 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Hooks.EwmhDesktops
 import System.IO
+import Data.Map.Lazy as M
+import XMonad.Actions.Submap (submap)
+import qualified XMonad.Util.ExtensibleState as XS
+import qualified Control.Concurrent.STM as STM
+import qualified System.Environment as Env
+
+commandSubMap xmobarPipeBox = submap (M.fromList [
+    ((0, xK_l), spawn "xscreensaver-command -lock"),
+    ((0, xK_d), toggleDPI xmobarPipeBox)
+  ])
+
+data KeyboardLayouts = KeyboardUS | KeyboardNeo deriving (Read, Show, Typeable)
+
+instance ExtensionClass KeyboardLayouts where
+        initialValue = KeyboardNeo
+        extensionType = PersistentExtension
+
+data DisplayDPI = DPI192 | DPI110 deriving (Read, Show, Typeable)
+
+instance ExtensionClass DisplayDPI where
+        initialValue = DPI192
+        extensionType = PersistentExtension
+
+toggleDpiValue DPI192 = DPI110
+toggleDpiValue DPI110 = DPI192
+
+dpiValue DPI192 = "192"
+dpiValue DPI110 = "110"
+
+scaleValues DPI192 = ("2", "0.5")
+scaleValues DPI110 = ("1", "1")
+
+setupDisplays DPI192 = spawn "xrandr --output eDP1 --auto --output DP1-1 --off            --output DP2-2 --off"
+setupDisplays DPI110 = spawn "xrandr --output eDP1 --off  --output DP2-2 --primary --auto --output DP1-1 --right-of DP2-2 --auto"
+
+setDPI :: (MonadIO m) => STM.TVar Handle -> String -> (String, String) -> m ()
+setDPI xmobarPipeBox value (gdkScale, gdkDpiScale) = do
+        spawn ("xrandr --dpi " ++ value)
+--        liftIO $ Env.setEnv "GDK_SCALE" gdkScale
+--        liftIO $ Env.setEnv "GDK_DPI_SCALE" gdkDpiScale
+        liftIO $ Env.unsetEnv "GDK_SCALE"
+        liftIO $ Env.unsetEnv "GDK_DPI_SCALE"
+        spawn ("xrdb -merge <<<\"Xft.dpi: " ++ value ++"\"")
+        newXmobar <- spawnPipe "xmobar ~/.xmobarrc"
+        oldPipe <- liftIO $ STM.atomically $ do
+                oldPipe <- STM.readTVar xmobarPipeBox
+                STM.writeTVar xmobarPipeBox newXmobar
+                return oldPipe
+        liftIO $ hClose oldPipe
+
+toggleDPI xmobarPipeBox = do
+        actual <- XS.get
+        let next = toggleDpiValue actual
+        setupDisplays next
+        setDPI xmobarPipeBox (dpiValue next) (scaleValues next)
+        XS.put next
+        refresh
+
+toggleKeyboardLayout = do
+        actual <- XS.get
+        case actual of
+                KeyboardNeo -> do
+                        spawn "setxkbmap us"
+                        XS.put KeyboardUS
+                        refresh
+                KeyboardUS -> do
+                        spawn "setxkbmap de neo"
+                        XS.put KeyboardNeo
+                        refresh
+
+getKeyboardLayout = do
+        actual <- XS.get :: X KeyboardLayouts
+        case actual of
+                KeyboardNeo -> return (Just (xmobarColor "red" "black" " NEO  "))
+                KeyboardUS -> return (Just (xmobarColor "green" "black" "  US  "))
+getDpiValue = do
+        actual <- XS.get :: X DisplayDPI
+        return (Just (xmobarColor "yellow" "black" ("DPI: " ++ dpiValue actual)))
+
 main = do
     xmproc <- spawnPipe "xmobar ~/.xmobarrc"
-    xmonad $ docks defaultConfig
-        { manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+    xmobarPipeBox <- STM.atomically $ STM.newTVar xmproc
+    let actual = initialValue
+    setupDisplays actual
+    setDPI xmobarPipeBox (dpiValue actual) (scaleValues actual)
+    xmonad $ docks def
+        { borderWidth = 5 
+        , manageHook = manageDocks <+> manageHook def
+        , layoutHook = avoidStruts  $  layoutHook def
         , handleEventHook = fullscreenEventHook
         , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = hPutStrLn xmproc
+                        { ppOutput = \line -> do
+                                xmproc <- STM.atomically $ STM.readTVar xmobarPipeBox
+                                hPutStrLn xmproc line
                         , ppTitle = xmobarColor "green" "" . shorten 50
+                        , ppExtras = ppExtras xmobarPP ++ [ getKeyboardLayout, getDpiValue ]
                         }
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
-        , terminal = "st -f 'Roboto Mono:pixelsize=18'"
+        , terminal = "st -f 'Roboto Mono:size=14'"
         }
+        `additionalKeys`
+        [ ((mod4Mask, xK_s), commandSubMap xmobarPipeBox),
+          ((mod4Mask, xK_BackSpace), toggleKeyboardLayout),
+          ((mod4Mask, xK_p), spawn "rofi -show run"),
+          ((mod4Mask, xK_o), spawn "rofi -show ssh")          
+          ]
