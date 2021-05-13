1182f87d1a4589b3d72224f9bff568098e50da03
Eric Wolf <eric@boese-wolf.eu>
2021-05-13 16:53:20 +0200

    Replace xmonad.hs under nixpkgs

--- nixpkgs/xmonad/xmonad.hs
+++ nixpkgs/xmonad/xmonad.hs
@@ -10,67 +10,22 @@ import XMonad.Actions.Submap (submap)
 import qualified XMonad.Util.ExtensibleState as XS
 import qualified Control.Concurrent.STM as STM
 import qualified System.Environment as Env
-import XMonad.Layout ((|||))
-import qualified XMonad.Layout.Tabbed as Tabbed
+import XMonad.Layout.Tabbed
+import XMonad.Config.Xfce
 
-commandSubMap xmobarPipeBox = submap (M.fromList [
+commandSubMap = submap (M.fromList [
     ((0, xK_l), spawn "xscreensaver-command -lock")
   ])
 
-data KeyboardLayouts = KeyboardUS | KeyboardNeo deriving (Read, Show, Typeable)
-
-instance ExtensionClass KeyboardLayouts where
-        initialValue = KeyboardNeo
-        extensionType = PersistentExtension
-
-toggleKeyboardLayout = do
-        actual <- XS.get
-        case actual of
-                KeyboardNeo -> do
-                        spawn "setxkbmap us"
-                        XS.put KeyboardUS
-                        refresh
-                KeyboardUS -> do
-                        spawn "setxkbmap de neo"
-                        XS.put KeyboardNeo
-                        refresh
-
-getKeyboardLayout = do
-        actual <- XS.get :: X KeyboardLayouts
-        case actual of
-                KeyboardNeo -> return (Just (xmobarColor "red" "black" " NEO  "))
-                KeyboardUS -> return (Just (xmobarColor "green" "black" "  US  "))
-startup :: X ()
-startup = do
-  spawn "trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --widthtype request --transparent true --heighttype pixel --height 36  --iconspacing 10  --tint 0x191970"
-  spawn "xscreensaver"
-  spawn "blueman-applet"
-  spawn "nm-applet"
-  spawn "xrdb -merge .Xresources"
---  spawn "yubioath-desktop"
-
 main = do
-    xmproc <- spawnPipe "xmobar ~/.xmobarrc"
-    xmobarPipeBox <- STM.atomically $ STM.newTVar xmproc
-    xmonad $ docks def
+    xmonad (xfceConfig
         { borderWidth = 5 
-        , manageHook = manageDocks <+> manageHook def
-        , layoutHook = avoidStruts  $  Tabbed.simpleTabbed ||| layoutHook def
-        , handleEventHook = fullscreenEventHook
-        , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = \line -> do
-                                xmproc <- STM.atomically $ STM.readTVar xmobarPipeBox
-                                hPutStrLn xmproc line
-                        , ppTitle = xmobarColor "green" "" . shorten 50
-                        , ppExtras = ppExtras xmobarPP ++ [ getKeyboardLayout ]
-                        }
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
-        , terminal = "st -f 'Roboto Mono:size=12'"
-        , startupHook = startup
+        , terminal = "st -f 'Roboto Mono:size=10'"
         }
         `additionalKeys`
-        [ ((mod4Mask, xK_s), commandSubMap xmobarPipeBox),
-          ((mod4Mask, xK_BackSpace), toggleKeyboardLayout),
+        [ ((mod4Mask, xK_s), commandSubMap),
           ((mod4Mask, xK_p), spawn "rofi -show run"),
           ((mod4Mask, xK_o), spawn "rofi -show ssh")          
           ]
+          )
